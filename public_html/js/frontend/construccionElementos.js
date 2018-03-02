527ae97007a3bcbfccdf4a2439b8ee2aa01e1d9f
Eduardo <eduguia2009@gmail.com>
2018-03-02 20:10:11 +0000

    Montada la estructura HTML de los objetos a mostrar
    Hago la prueba con el ejemplo de Armando,
    pero la estructura esta comentada al final del html

--- public_html/js/frontend/construccionElementos.js
+++ public_html/js/frontend/construccionElementos.js
@@ -4,7 +4,7 @@ function construccion(arrayObjetosVenta){
     }
 }
 function construyeElemento(objetoVenta){
-    let text = '<div>';
+    /*let text = '<div>';
     text += "<p><strong>ID:</strong> "+objetoVenta.id+"</p>";
     text += "<p><strong>Nombre:</strong> "+objetoVenta.nombre+"</p>";
     text += "<p><strong>Imagen grande:</strong> "+objetoVenta.imagenGrande+"</p>";
@@ -17,6 +17,28 @@ function construyeElemento(objetoVenta){
     text += "<p><strong>Descripción corta:</strong> "+objetoVenta.descripcionCorta+"</p>";
     text += "<p><strong>Descripción:</strong> "+objetoVenta.descripcion+"</p>";
     text += "<p><strong>Tienda:</strong> "+objetoVenta.tienda+"</p>";
-    text += '</div>';
-    $('#contenidos').append(text);
+    text += '</div>';*/
+    
+    var text = `<div class="producto">
+                <div class="cabezaproducto">
+                    <img id="ProductoImagen" src="${objetoVenta.imagen}" alt="imagen"/>            
+                    <img class="Tipoproducto" src="addons/images/ebay_icon.svg" alt=""/>
+                </div>
+                <div class="ProductoInfo">
+                    <div id="Productonombre">${objetoVenta.nombre}</div>
+                    <div id="Productodescripcion">${objetoVenta.descripcionCorta}</div>
+                    <p>
+                        <label>Puntuación: </label>
+                        <label id="Productopuntuacion">${objetoVenta.puntuacion} puntos</label>                        
+                    </p>
+                    <p>
+                        <label>Precio:</label>
+                        <div class="precioCarrito">
+                            <label id="Productoprecio" class="sombraTexto">${objetoVenta.precio} €</label>
+                            <img id="addItem" src="addons/icons/add_item.svg" alt=""/>                     
+                        </div>                        
+                    </p>
+                </div>                
+            </div>`;
+    $('#resultado').append(text);
 }
