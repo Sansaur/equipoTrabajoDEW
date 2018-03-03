848316bd4e4625b1ffe0376a393fe566edae6377
Sansaur <arsansua@gmail.com>
2018-03-03 10:31:45 +0000

    Update construccionElementos.js

--- js/frontend/construccionElementos.js
+++ js/frontend/construccionElementos.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 */
 function getFiltros(){
     $.ajax({
-        url:"/public_html/js/backend/asociacionCategoriasNuevo.json",
+        url:"/js/backend/asociacionCategoriasNuevo.json",
         dataType: "JSON",
         success:function(data){
             _data = data;
